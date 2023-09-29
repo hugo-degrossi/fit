46d6e7372ab492e8c5780605a3d4911fe27a8451
hugo-degrossi <contact@hugo-degrossi.fr>
2023-09-30 01:03:40 +0200

    add header to edit circuit view

--- lib/pages/circuit_edit_page.dart
+++ lib/pages/circuit_edit_page.dart
@@ -1,3 +1,4 @@
+import 'package:fit/themes/fit_app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CircuitEditPage extends StatelessWidget {
@@ -5,15 +6,34 @@ class CircuitEditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final FitAppColors fitAppColors =
+        Theme.of(context).extension<FitAppColors>()!;
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
             Navigator.pop(context);
           },
-          child: const Text('Go back!'),
+          child: Icon(
+            Icons.arrow_back,
+            size: 28,
+            color: Theme.of(context).colorScheme.tertiary,
+          ),
         ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        elevation: 0,
       ),
+      body: Column(children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          width: double.infinity,
+          color: Theme.of(context).colorScheme.primary,
+          child: Text(
+            "Un Exercice".toUpperCase(),
+            style: Theme.of(context).textTheme.displayMedium,
+          ),
+        )
+      ]),
     );
   }
 }
